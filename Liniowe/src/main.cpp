d0d3dea1ea7807f367b1b03bd271c3081b003506
jbryl7 <37078470+jbryl7@users.noreply.github.com>
2018-12-13 23:53:09 +0100

    Test for statistics implemented
    tests:
    -appendTime
    -prependTime
    -eraseTime
    scores are saved in a file text.txt

--- Liniowe/src/main.cpp
+++ Liniowe/src/main.cpp
@@ -1,28 +1,81 @@
 #include <cstddef>
 #include <cstdlib>
 #include <string>
-
+#include <initializer_list>
+#include <iostream>
+#include <fstream>
 #include "Vector.h"
 #include "LinkedList.h"
+#define PRINTTIME x=clock()-time;file<<x<<";"
+using namespace aisdi;
 
-namespace
+    std::fstream file;
+/*
+LinkedList<int> * createLinkedList(size_t numberOfElements)
 {
-
-template <typename T>
-using LinearCollection = aisdi::LinkedList<T>;
-
-void perfomTest()
+    *linkedList = new LinkedList;
+    return linkedList
+}
+Vector<int> * createVector(size_t numberOfElements)
 {
-  LinearCollection<std::string> collection;
-  collection.append("TODO");
+    *vector = new Vector;
+    for( size_t i = 0; i < numberOfElements; i++)
+        vector->append((const int) i);
+    return vector;
+}*/
+
+void prependTest(Vector<int> *vec, LinkedList<int> *llist)
+{
+    double x;    
+    file<<vec->getSize()<<";";
+    clock_t time  = clock();
+    vec->prepend(10);
+    PRINTTIME;
+    time = clock();
+    llist->prepend(10);
+    PRINTTIME;
 }
 
-} // namespace
-
-int main(int argc, char** argv)
+void appendTest(Vector<int> *vec, LinkedList<int> *llist)
 {
-  const std::size_t repeatCount = argc > 1 ? std::atoll(argv[1]) : 10000;
-  for (std::size_t i = 0; i < repeatCount; ++i)
-    perfomTest();
-  return 0;
+    double x;
+    clock_t time = clock();
+    vec->append(15);
+    PRINTTIME;
+    time = clock();
+    llist->append(15);
+    PRINTTIME;
+}
+void eraseTest(Vector<int> *vec, LinkedList<int> *llist)
+{
+double x;
+    auto vecIter = vec->cbegin();
+    clock_t time = clock();
+    vec->erase(vecIter);
+    PRINTTIME;
+
+    auto listIter = llist->cbegin();
+    time = clock();
+    llist->erase(listIter);
+    PRINTTIME;
+}
+
+
+int main()
+{
+file.open("test.txt", std::fstream::out);
+LinkedList<int> *linkedList = new LinkedList<int>;
+Vector<int> *vect = new Vector<int>;
+file<<"items;vectorPrependTime;linkedListPrependTime;VectorAppendTime;LinkedListAppendTime;VectorEraseTime;LinkedListEraseTime;\n";
+    for(size_t amount=1; amount<=100000 ; amount++)
+    {
+        prependTest(vect, linkedList);
+        appendTest(vect, linkedList);
+        eraseTest(vect, linkedList);
+        file<<"\n";
+    }
+    file.close();
+    delete linkedList;
+    delete vect;
+    return 0;
 }
