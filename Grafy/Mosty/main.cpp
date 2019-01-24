9a946a5f798d4274c0e12e77500c03734721a65b
jbryl7 <37078470+jbryl7@users.noreply.github.com>
2019-01-24 19:51:22 +0100

    Rewritten

--- Grafy/Mosty/main.cpp
+++ Grafy/Mosty/main.cpp
@@ -1,30 +1,44 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
-#include "grafy.h"
+#include "./Graph.h"
 #include <fstream>
 
-int main()
-{
-    int x,y;
-    ifstream file;
-    file.open("data.txt");
-    if (!file.good())
-        cout<<"Error while entering file"<<endl;
-    
-    Graph graph;
+int main(int argc, char* argv[]) {
+    std::fstream graphFile;
 
-    while(!file.eof())
-    {
-        file>>x>>y;
-        graph.add_element(x,y);   
+    int vertice1, vertice2;
+    int sizeOfGivenGraph;
+    if (argc < 2){
+        std::cout<<"No arguments given\n";
+        return 1;
     }
-    file.close();
 
-    graph.printAllEdges();
-    cout<<"\n\n\n";
-    
-    graph.printAllRozlegleMosty();
+    for (int i = 1; i < argc; i++) {
+        if (argc != 2)
+            std::cout<<"test"<<i<<'\n';
+        std::string fileName = argv[i];
+        graphFile.open(fileName);
+        if (!graphFile.good()) {
+            std::cout << "File not found\n";
+            return 3;
+        }
 
+        graphFile >> sizeOfGivenGraph;
+        if (sizeOfGivenGraph < 4)
+            break;
+        Graph graph(sizeOfGivenGraph);
+        while (!graphFile.eof()) {
+            graphFile >> vertice1 >> vertice2;
+            graph.addEdge(vertice1, vertice2);
+        }
+
+        graphFile.close();
+        graph.printEdges();
+        clock_t begin = clock();
+        graph.printBridges();
+        clock_t end = clock();
+        std::cout << "\nTime: " << double(end - begin) / CLOCKS_PER_SEC << "\n\n";
+    }
     return 0;
 }
